b781e3c1e04327db2aeeae0866a740366aeb2e4f
보현 김 <nhs04047@gmail.com>
2022-08-17 21:27:30 +0900

    feat : comments 스키마 정의

---
+++ src/comments/comments.schema.ts
@@ -0,0 +1,59 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { IsEmail, IsNotEmpty, IsPositive, IsString } from 'class-validator';
+import { Document, SchemaOptions, Types } from 'mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+
+const options: SchemaOptions = {
+  timestamps: true,
+};
+
+@Schema(options)
+export class Comments extends Document {
+  @ApiProperty({
+    description: '작성한 고양이 id',
+    required: true,
+  })
+  @Prop({
+    type: Types.ObjectId,
+    required: true,
+    ref: 'cats',
+  })
+  @IsNotEmpty()
+  author: string;
+
+  @ApiProperty({
+    description: '댓글 컨텐츠',
+    required: true,
+  })
+  @Prop({
+    required: true,
+  })
+  @IsNotEmpty()
+  @IsString()
+  contents: string;
+
+  @ApiProperty({
+    description: '좋아요 수',
+  })
+  @Prop({
+    required: true,
+    default: 0,
+  })
+  @IsString()
+  @IsPositive()
+  likeCount: number;
+
+  @ApiProperty({
+    description: '작성 대상 (게시물, 정보글)',
+    required: true,
+  })
+  @Prop({
+    type: Types.ObjectId,
+    required: true,
+    ref: 'cats',
+  })
+  @IsNotEmpty()
+  info: string;
+}
+
+export const CommentsScema = SchemaFactory.createForClass(Comments);
